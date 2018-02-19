e3be3013dade6555a1273907cd4c617b061d82e7
Joao Moura <joao.moura@b-i.com>
2018-02-19 23:11:45 +0000

    Added sequelize, changed code for es2017

--- app.js
+++ app.js
@@ -6,46 +6,52 @@ import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 
 // routes
-import index from './routes/index'
-import users from './routes/users'
+import index from 'routes/index'
+import users from 'routes/users'
 
-const app = express()
+class App {
+  constructor() {
+    this.server = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+    // view engine setup
+    this.server.set('views', path.join(__dirname, 'views'))
+    this.server.set('view engine', 'jade')
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    this.server.use(logger('dev'))
+    this.server.use(bodyParser.json())
+    this.server.use(bodyParser.urlencoded({ extended: false }))
+    this.server.use(cookieParser())
+    this.server.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index)
-app.use('/users', users)
+    this.server.use('/', index)
+    this.server.use('/users', users)
 
-// catch 404 and forward to error handler
-app.use(
-  (req, res, next) => {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    // catch 404 and forward to error handler
+    this.server.use(
+      (req, res, next) => {
+        var err = new Error('Not Found')
+        err.status = 404
+        next(err)
+      }
+    );
+
+    // error handler
+    this.server.use(
+      (err, req, res, next) => {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+        // render the error page
+        res.status(err.status || 500)
+        res.render('error')
+      }
+    )
   }
-);
+}
 
-// error handler
-app.use(
-  (err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+const app = new App()
 
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-  }
-)
-
-module.exports = app
+export default app
